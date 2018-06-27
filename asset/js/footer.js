02b8e9e1dd9f0b2c1d0bef7396fcd8b5b9874de0
e <sober_e1@qq.com>
2018-06-27 18:00:57 +0800

    解决bug

--- asset/js/footer.js
+++ asset/js/footer.js
@@ -1,29 +1,28 @@
 $(document).ready(function () {
     var nDocWidth = $(document).width(),
-        rightNowWidth = nDocWidth;
-    fContent = $('.f-content'),
+        fContent = $('.f-content'),
         fTitle = $(".f-title"),
         fBtn = $(".f-btn");
     fBtn.hide();
     $(".navbar-toggler").click(function () {
-        $(this).toggleClass("isactive")
+        $(this).toggleClass("isactive");
     })
 
     // 检测浏览器变化 控制底部显示隐藏 屏幕宽度小于768px就隐藏加上个按钮
     $(window).resize(function () {
         var docWidth = $(document).width();
-        rightNowWidth = docWidth;
         checkDocumentWidth(docWidth);
     })
     // 直接判断当前浏览器宽度
     function checkDocumentWidth(width) {
         if (width <= 570) {
-            fContent.hide();
+            if (!fContent.is(":hidden")) {
+                fContent.hide();
+            }
             fBtn.show();
             fTitle.addClass("f-title-sp");
 
         } else {
-            fTitle.bind("click");
             fContent.show();
             fContent.removeClass("f-content-sp");
             fTitle.removeClass("f-title-sp");
@@ -32,7 +31,7 @@ $(document).ready(function () {
     }
     checkDocumentWidth(nDocWidth);
     fTitle.on("click", function () {
-        if (rightNowWidth <= 550) {
+        if (nDocWidth <= 550) {
             $(this).next(".f-content").toggleClass("f-content-sp");
             $(this).next(".f-content").toggle("0.5s");
         } else {
