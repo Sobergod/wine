028ff1f2234249f284ca1690709597657a9e95dd
e <sober_e1@qq.com>
2018-06-27 10:55:53 +0800

    底部样式100%

---
+++ js/footer.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    var nDocWidth = $(document).width(),
+        rightNowWidth = nDocWidth;
+    fContent = $('.f-content'),
+        fTitle = $(".f-title"),
+        fBtn = $(".f-btn");
+    fBtn.hide();
+    $(".navbar-toggler").click(function () {
+        $(this).toggleClass("isactive")
+    })
+
+    // 检测浏览器变化 控制底部显示隐藏 屏幕宽度小于768px就隐藏加上个按钮
+    $(window).resize(function () {
+        var docWidth = $(document).width();
+        rightNowWidth = docWidth;
+        checkDocumentWidth(docWidth);
+    })
+    // 直接判断当前浏览器宽度
+    function checkDocumentWidth(width) {
+        if (width <= 550) {
+            fContent.hide();
+            fBtn.show();
+            fTitle.addClass("f-title-sp");
+            // fTitle.unbind("click");
+
+        } else {
+            fTitle.bind("click");
+            fContent.show();
+            fTitle.removeClass("f-title-sp");
+            fBtn.hide()
+        }
+    }
+    checkDocumentWidth(nDocWidth);
+    fTitle.on("click", function () {
+        if (rightNowWidth <= 550) {
+            $(this).next(".f-content").toggleClass("f-content-sp");
+            $(this).next(".f-content").toggle("0.5s");
+        } else {
+            return false;
+        }
+    })
+})
